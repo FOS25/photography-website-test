893dbf2e3aece469876efa208a95e67c63c15e02
FOS25 <K00299293@STUDENT.TUS.IE>
2025-05-16 12:28:18 +0100

    Updated script.js with new features

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
 document.addEventListener("DOMContentLoaded", () => {
     console.log("Photography site loaded!");
 });
+document.addEventListener("DOMContentLoaded", () => {
+    const images = document.querySelectorAll("#gallery img");
+    images.forEach(img => {
+        img.style.opacity = "0";
+        img.style.transition = "opacity 1s";
+        setTimeout(() => img.style.opacity = "1", 500);
+    });
+});
+
 
